b54e58f32194c411d4a44a0950c67e2e9193e354
Ke-Fu Shen <34981744+Tofu2018@users.noreply.github.com>
2018-11-24 20:14:17 +0800

    Add files via upload
    20181124:first change make up

---
+++ huffman/function.h
@@ -0,0 +1,25 @@
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void input(){
+	string a;
+	while(cin >> a){
+
+
+
+
+
+
+
+
+
+
+
+
+		cout << a;
+	}
+}
---
+++ huffman/main.cpp
@@ -0,0 +1,11 @@
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "function.h"
+
+using namespace std;
+
+int main(){
+	input();
+}
