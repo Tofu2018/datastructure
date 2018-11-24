5c79007c1f72b49d0d071f19803f8a75f7e0e86d
Ke-Fu Shen <34981744+Tofu2018@users.noreply.github.com>
2018-11-24 20:19:40 +0800

    Add files via upload

--- huffman/function.h
+++ huffman/function.h
@@ -6,20 +6,23 @@
 using namespace std;
 
 void input(){
+	ofstream analyze;
+	analyze.open(file/analyze/example.txt,ios::app);
+	if(analyze.fail()){
+		system("cd. ")
+	}
+	
 	string a;
 	while(cin >> a){
-
-
-
-
-
-
-
-
-
-
-
-
-		cout << a;
+		if(a!='.'){
+			cout << "End Input!!" << endl;
+			return 0;
+		}else{
+			
+		}
 	}
+}
+
+void treemaker(){
+	
 }
